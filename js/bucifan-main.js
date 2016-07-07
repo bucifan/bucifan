6aab3ead61f689e698aa2363a5fa0f29a47aef12
bucifan <roberoach@hotmail.com>
2016-07-07 09:15:14 -0400

    all schedule

--- js/bucifan-main.js
+++ js/bucifan-main.js
@@ -1,8 +1,28 @@
 function loadScheduleItems(){
     var game1 = $("#gameitem1");
     var game2 = $("#gameitem2");
+    var game3 = $("#gameitem3");
+    var game4 = $("#gameitem4");
+    var game5 = $("#gameitem5");
+    var game6 = $("#gameitem6");
+    var game7 = $("#gameitem7");
+    var game8 = $("#gameitem8");
+    var game9 = $("#gameitem9");
+    var game10 = $("#gameitem10");
+    var game11 = $("#gameitem11");
+    var game12 = $("#gameitem12");
     TweenLite.to(game1,3,{opacity:1.0});
     TweenLite.to(game2,3,{opacity:1.0, delay:2});
+    TweenLite.to(game3,3,{opacity:1.0, delay:3});
+    TweenLite.to(game4,3,{opacity:1.0, delay:4});
+    TweenLite.to(game5,3,{opacity:1.0, delay:5});
+    TweenLite.to(game6,3,{opacity:1.0, delay:6});
+    TweenLite.to(game7,3,{opacity:1.0, delay:7});
+    TweenLite.to(game8,3,{opacity:1.0, delay:8});
+    TweenLite.to(game9,3,{opacity:1.0, delay:9});
+    TweenLite.to(game10,3,{opacity:1.0, delay:10});
+    TweenLite.to(game11,3,{opacity:1.0, delay:11});
+    TweenLite.to(game12,3,{opacity:1.0, delay:12});
 }
 /*
 $(document).on('ready', function () {
