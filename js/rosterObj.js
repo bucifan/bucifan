387c4da8e29cfe8c01d07ddd79d9e462a83a778f
Rob Roach <roberoach@hotmail.com>
2016-09-07 14:01:13 +0000

    o line

--- js/rosterObj.js
+++ js/rosterObj.js
@@ -32,7 +32,6 @@ var players = [
     {TwoDeep: 1, OffDef: "D", name: "Nick Conner", pos: "LB", el:"RSFR", number:34, from:"Dublin | Scioto", hgt:6.3 , wgt:230},
     {TwoDeep: 1, OffDef: "D", name: "Chris Worley", pos: "LB", el:"RSJR", number:35, from:"Cleveland | Glenville", hgt:6.2 , wgt:225},
     {TwoDeep: 1, OffDef: "D", name: "Jerome Baker", pos: "LB", el:"SO", number:17, from:"Cleveland | St. Benedictine", hgt:6.2 , wgt:215},
-    {TwoDeep: 1, OffDef: "D", name: "Rashod Baker", pos: "LB", el:"RSFR", number:13, from:"Lorain | Lorain", hgt:6.4 , wgt:252},
     {TwoDeep: 1, OffDef: "D", name: "Craig Fada", pos: "LB", el:"SR", number:38, from:"Columbus | Waterson", hgt:6.1 , wgt:225},
     {TwoDeep: 1, OffDef: "D", name: "Gareon Conley", pos: "CB", el:"RSJR", number:8, from:"Massillon | Washington", hgt:6.0 , wgt:195},
     {TwoDeep: 1, OffDef: "D", name: "Damon Arnette", pos: "CB", el:"RSFR", number:3, from:"Fort Lauderdale, Fla. | St. Thomas Aquinas", hgt:6.0 , wgt:191},
@@ -43,9 +42,22 @@ var players = [
     {TwoDeep: 1, OffDef: "D", name: "Marshon Lattimore", pos: "CB", el:"RSSO", number:2, from:"Cleveland | Glenville", hgt:6.0 , wgt:195},
     {TwoDeep: 1, OffDef: "D", name: "Denzel Ward", pos: "CB", el:"SO", number:12, from:"Macedonia | Nordonia", hgt:5.11 , wgt:175},
     {TwoDeep: 0, OffDef: "D", name: "Joshua Albi", pos: "DT", el:"RSFR", number:58, from:"Detroit | Cass Tech", hgt:6.5 , wgt:295},
-    {TwoDeep: 0, OffDef: "O", name: "A.J. Alexander", pos: "TE", el:"RSFR", number:88, from:"Burke, Va. | Lake Braddock", hgt:6.2 , wgt:254},
+    {TwoDeep: 1, OffDef: "O", name: "A.J. Alexander", pos: "TE", el:"RSFR", number:88, from:"Burke, Va. | Lake Braddock", hgt:6.2 , wgt:254},
     {TwoDeep: 0, OffDef: "O", name: "Alex Backenstoe", pos: "RB", el:"SO", number:35, from:"Hilliard | Davidson", hgt:6.0 , wgt:220},
-    {TwoDeep: 0, OffDef: "D", name: "Jarrod Barnes", pos: "SAF", el:"GS", number:26, from:"Westerville | South", hgt:6.0 , wgt:200}
-]
+    {TwoDeep: 0, OffDef: "D", name: "Jarrod Barnes", pos: "SAF", el:"GS", number:26, from:"Westerville | South", hgt:6.0 , wgt:200},
+    {TwoDeep: 1, OffDef: "O", name: "K.J. Hill", pos: "WR", el:"FR", number:14, from:"North Little Rock, Ark. | North Little Rock", hgt:6.0 , wgt:200},
+    {TwoDeep: 1, OffDef: "D", name: "Rashod Berry", pos: "DE", el:"FR", number:13, from:"Lorain | Lorain", hgt:6.4 , wgt:252},
+    {TwoDeep: 1, OffDef: "D", name: "Jordan Fuller", pos: "FS", el:"FR", number:4, from:"Old Tappan, N.J. | Old Tappan", hgt:6.2 , wgt:205},
+    {TwoDeep: 1, OffDef: "O", name: "Jamarco Jones", pos: "OT", el:"JR", number:74, from:"Chicago Il | De LasSalle", hgt:6.5 , wgt:310},
+    {TwoDeep: 1, OffDef: "O", name: "Evan Lisle", pos: "OT", el:"JR", number:75, from:"Centerville | Centerville", hgt:6.6 , wgt:308},
+    {TwoDeep: 1, OffDef: "O", name: "Kevin Woidke", pos: "OT", el:"SO", number:63, from:"Cleveland | St. Ignasius", hgt:6.6 , wgt:295},
+    {TwoDeep: 1, OffDef: "O", name: "Michael Jordan", pos: "OG", el:"FR", number:73, from:"Canton Mich. | Plymoth", hgt:6.7 , wgt:310},
+    {TwoDeep: 1, OffDef: "O", name: "Matthew Burrell", pos: "OG", el:"FR", number:69, from:"Woodbridge Va. | Hylton", hgt:6.4 , wgt:305},
+    {TwoDeep: 1, OffDef: "O", name: "Pat Elflein", pos: "C", el:"SR", number:65, from:"Pickerington | Pickerington North", hgt:6.3 , wgt:300},
+    {TwoDeep: 1, OffDef: "O", name: "Brady Taylor", pos: "C", el:"SO", number:79, from:"Columbus | Bishop Ready" hgt:6.5 , wgt:300},
+    {TwoDeep: 1, OffDef: "O", name: "Billy Price", pos: "OG", el:"JR", number:54, from:"Austintown | Fitch", hgt:6.4 , wgt:315},
+    {TwoDeep: 1, OffDef: "O", name: "Demetrius Knox", pos: "OG", el:"SO", number:78, from:"Fort Worth, TX | All Saints Episcopal ", hgt:6.4 , wgt:308},
+    {TwoDeep: 1, OffDef: "O", name: "Isiah Price", pos: "OT", el:"SO", number:59, from:"Greenbelt, Md. | Eleanor Roosevelt", hgt:6.7 , wgt:310},
+    {TwoDeep: 1, OffDef: "O", name: "Brandon Bowen", pos: "OT", el:"RSFR", number:76, from:"Draper, Utah (Corner Canyon)", hgt:6.7 , wgt:315}
 
 //    {TwoDeep: 0, OffDef: "O", name: "", pos: "", el:"", number:, from:" | ", hgt: , wgt:},
