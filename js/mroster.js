83f99f613321163f9cf07eaa858c20b1655697e6
Rob Roach <roberoach@hotmail.com>
2016-09-28 16:52:01 +0000

    mobile roster

--- js/mroster.js
+++ js/mroster.js
@@ -1,3 +1,7 @@
+players.sort(function(a,b){
+   return a.number - b.number; 
+});
+
 for(var i=0;i<players.length;i++){
-  $("#mblRoster").append("<tr><td>"+ players[i].number+"</td><td>"+ players[i].name+"</td><td>"+ players[i].el+"</td></tr>");
+  $("#mblRoster").append("<tr><td>"+ players[i].number+"</td><td>"+ players[i].name+"</td><td>"+ players[i].pos+"</td><td>"+ players[i].el+"</td></tr>");
 }  
