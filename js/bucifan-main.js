f08df8655880dfd655051822e49f5b068711430a
Rob Roach <roberoach@hotmail.com>
2016-10-03 17:35:47 +0000

    adding tdls

--- js/bucifan-main.js
+++ js/bucifan-main.js
@@ -43,9 +43,9 @@ $.getJSON("https://bucifan-api.azurewebsites.net/osugames")
                 worl = "<b style='color:red'> L </b>"; 
              }
              $("#gameitem"+i).append("<span class='schtime' style='color:black' >"+worl+" OSU: " + games.y2016[i].OSUScore + " " + games.y2016[i].OppShort + ": "+games.y2016[i].OppScore+"</span>"); 
-             $("#gamedtl"+i).append("<div><b> Total Yards: </b> " + games.y2016[i].tyards + "<br/><b> Passing Yards: </b> " + games.y2016[i].pyards + "<br/><b> Rushing Yards: </b> " + games.y2016[i].ryards + "<br/></div>");
-             $("#gamedtl"+i).append("<div><b> Average Yards per play: </b> " + games.y2016[i].ypp + "<br/><b> Yards given up: </b> " + games.y2016[i].ygivenup + "<br/></div>");
-             $("#gamedtl"+i).append("<div style='width:100%><b> Notes: </b> " + games.y2016[i].mynotes + "<br/></div>");
+             $("#gamedtl"+i).append("<div style='float:left'><b> Total Yards: </b> " + games.y2016[i].tyards + "<br/><b> Passing Yards: </b> " + games.y2016[i].pyards + "<br/><b> Rushing Yards: </b> " + games.y2016[i].ryards + "<br/></div>");
+             $("#gamedtl"+i).append("<div style='float:right'><b> Average Yards per play: </b> " + games.y2016[i].ypp + "<br/><b> Yards given up: </b> " + games.y2016[i].ygivenup + "<br/></div>");
+             $("#gamedtl"+i).append("<div style='width:100%'><b> Notes: </b> " + games.y2016[i].mynotes + "<br/></div>");
           }
           
       } 
